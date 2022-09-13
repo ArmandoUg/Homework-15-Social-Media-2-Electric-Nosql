3b00d97d7a382d9880d9270a90335cead0437cbf
Armando Ugalde <uarmando15@yahoo.com>
2022-09-12 21:06:42 -0700

    added a get thoughts and post thoughts route

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -3,12 +3,27 @@ const router = require('express').Router();
 const { Thought, Reaction} = require('../../models')
 
 //TODO: ROUTE TO GET ALL THOUGHTS
-router.get('/', (req,res)=> {
+router.get('/', async (req,res)=> {
+    try{
+        let thoughtData = await Thought.find({})
+        res.status(200).json(thoughtData)
+    }
+    catch(err){
+        res.status(500).json(err)
+    }
 
 })
 
 //TODO: ROUTE TO CREATE A NEW THOUGHT
-router.post('/', (req,res)=> {
+router.post('/', async (req,res)=> {
+    try{
+        let thoughtData = await Thought.create(req.body)
+        res.status(200).json(thoughtData)
+    }
+    catch(err){
+        res.status(500).json(err)
+    }
+
 
 });
 
