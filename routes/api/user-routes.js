982bb028d9660c622980143d6c9622603129ff7b
Armando Ugalde <uarmando15@yahoo.com>
2022-09-14 20:07:51 -0700

    added routes for updating and deleting users, added a route for adding friends and removing friends

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -36,23 +36,47 @@ router.get('/:userId', async (req,res) => {
 })
 
 //TODO - ROUTE THAT UPDATES A SINGLE USER
-router.put('/:userId', (req,res)=> {
-
+router.put('/:userId', async (req,res)=> {
+    try{
+        let userData = await User.findOneAndUpdate({_id: req.params.userId}, req.body, {new: true})
+        res.status(200).json("User updated")
+    }
+    catch(err){
+        res.status(500).json(err)
+    }
 })
 
 //TODO - ROUTE THAT DELETES A SINGLE USER BASED ON USER ID
-router.delete('/:userId', (req,res)=> {
-
+router.delete('/:userId', async (req,res)=> {
+    try{
+        let userData = await User.deleteOne({_id: req.params.userId})
+        res.status(200).json("User deleted")
+    }
+    catch(err){
+        res.status(500).json(err)
+    }
 });
 
 //TODO - ROUTE THAT ADDS A FRIEND TO A USER
-router.put('/:userId/friends/:friendId', (req,res)=> {
-
+router.put('/:userId/friends/:friendId', async (req,res)=> {
+    try{
+        let userData = await User.findOneAndUpdate({_id: req.params.userId}, {$push: {friends: req.params.friendId}}, {new: true})
+        res.status(200).json(userData)
+    }
+    catch(err){
+        res.status(500).json(err)
+    }
 })
 
 //TODO - ROUTE THAT DELETES A FRIEND FROM A USER'S FRIENDS, DONT DELETE THE FRIEND AS A USER THOUGH!
-router.delete('/:userId/friends/:friendId', (req,res)=> {
-  
+router.delete('/:userId/friends/:friendId', async (req,res)=> {
+    try{
+        let userData = await User.findOneAndUpdate({_id: req.params.userId}, {$pull: {friends: req.params.friendId}}, {new: true})
+        res.status(200).json(userData)
+    }
+    catch(err){
+        res.status(500).json(err)
+    }
 });
 
 module.exports = router;
